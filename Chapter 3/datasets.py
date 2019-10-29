ab5f967e3ec5a207246d0466126819121294677d
djfan <df1676@nyu.edu>
2019-10-29 12:26:23 -0400

    (placeholder for link) to be updated

---
+++ Chapter 3/datasets.py
@@ -0,0 +1,16 @@
+import geopandas as gpd
+from cartoframes.auth import set_default_credentials
+from cartoframes.data import Dataset
+
+set_default_credentials('ebook-sds')
+
+def get_retail_store_minnesota():
+    """Retrieve Retail Store Locations in Minnesota
+    
+    Returns:
+        geopandas.GeoDataFrame: GeoDataFrame representation of table
+    """
+    table_name = 'retail_store_minnesota'
+    data = gpd.GeoDataFrame(Dataset(table_name).download(decode_geom=True))
+    data.crs = {'init': 'epsg:4326'}
+    return data
