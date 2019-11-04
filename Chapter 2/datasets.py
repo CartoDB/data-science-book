d85e4a41e4dd7464ebfffc207eeeb7ef226da14a
Andy Eschbacher <andyesch@protonmail.com>
2019-11-04 15:43:09 -0500

    adds dbscan notebook

--- Chapter 2/datasets.py
+++ Chapter 2/datasets.py
@@ -2,15 +2,37 @@ import geopandas as gpd
 from cartoframes.auth import set_default_credentials
 from cartoframes.data import Dataset
 
-set_default_credentials('ebook-sds')
+set_default_credentials("ebook-sds")
 
-def get_nyc_census_tracts():
-    """Retrieve dataset on NYC Census Tracts
-    
+
+def get_table(tablename):
+    """Retrieve tablename as a GeoDataFrame ordered by database id
+
     Returns:
         geopandas.GeoDataFrame: GeoDataFrame representation of table
     """
-    base_query = 'SELECT * FROM census_tracts_cleaned ORDER BY cartodb_id ASC'
+    base_query = ("SELECT * FROM {tablename} ORDER BY cartodb_id ASC").format(
+        tablename=tablename
+    )
     data = gpd.GeoDataFrame(Dataset(base_query).download(decode_geom=True))
-    data.crs = {'init': 'epsg:4326'}
+    data.crs = {"init": "epsg:4326"}
     return data
+
+
+def get_nyc_census_tracts():
+    """Retrieve dataset on NYC Census Tracts
+
+    Returns:
+        geopandas.GeoDataFrame: GeoDataFrame representation of table
+    """
+    return get_table("census_tracts_cleaned")
+
+
+def get_safegraph_visits():
+    """Retrieve Safegraph visit data for Panama City Beach in July 2019
+    as a GeoDataFrame ordered by database id
+
+    Returns:
+        geopandas.GeoDataFrame: GeoDataFrame representation of table
+    """
+    return get_table("safegraph_pcb_visits")
